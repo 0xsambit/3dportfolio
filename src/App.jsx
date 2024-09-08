91a789fd1dae8cae5c10595d0602e2cbc208dcff
0xsambit <177391633+0xsambit@users.noreply.github.com>
2024-09-08 12:51:53 +0530

    Initial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from "./sections/Hero";
+import Navbar from "./sections/Navbar";
+
+const App = () => {
+	return (
+		<main>
+			<Navbar />
+			<Hero />
+		</main>
+	);
+};
+
+export default App;
