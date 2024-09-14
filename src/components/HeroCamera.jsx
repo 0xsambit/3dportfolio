6d70e24fa68cc7fc7bc66f1f6ea48829181a7ac9
0xsambit <177391633+0xsambit@users.noreply.github.com>
2024-09-14 13:52:21 +0530

    Refactor Cube component to use GLTF loader and GSAP animation

---
+++ src/components/HeroCamera.jsx
@@ -0,0 +1,12 @@
+import { useFrame } from "@react-three/fiber";
+import { easing } from "maath";
+import React, { useRef } from "react";
+const HeroCamera = ({ children }) => {
+	const groupRef = useRef();
+	useFrame((state, delta) => {
+		easing.damp3(state.camera.position, [0, 0, 20], 0.25, delta);
+	});
+	return <group ref={groupRef}>{children}</group>;
+};
+
+export default HeroCamera;
