84a48a2d7f66a74c63915c657625bd8b960f7a49
0xsambit <177391633+0xsambit@users.noreply.github.com>
2024-09-14 11:44:51 +0530

    Refactor Target component to use GLTF loader and GSAP animation

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,46 +3,16 @@ import { Canvas } from "@react-three/fiber";
 import HackerRoom from "../components/HackerRoom";
 import { Suspense } from "react";
 import CanvasLoader from "../components/CanvasLoader";
-import { Leva, useControls } from "leva";
+import { useMediaQuery } from "react-responsive";
+import { calculateSizes } from "../constants/index";
+import Target from "../components/Target";
 
 const Hero = () => {
-	const x = useControls("HackerRoom", {
-		positionX: {
-			value: 2.5,
-			min: -10,
-			max: 10,
-		},
-		positionY: {
-			value: 2.5,
-			min: -10,
-			max: 10,
-		},
-		positionZ: {
-			value: 2.5,
-			min: -10,
-			max: 10,
-		},
-		rotationX: {
-			value: 0,
-			min: -10,
-			max: 10,
-		},
-		rotationY: {
-			value: 0,
-			min: -10,
-			max: 10,
-		},
-		rotationZ: {
-			value: 0,
-			min: -10,
-			max: 10,
-		},
-		scale: {
-			value: 1,
-			min: 0.1,
-			max: 10,
-		},
-	});
+	const isSmall = useMediaQuery({ maxWidth: 440 });
+	const isMobile = useMediaQuery({ maxWidth: 768 });
+	const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
+
+	const sizes = calculateSizes(isSmall, isMobile, isTablet);
 	return (
 		<section className='min-h-screen w-full flex flex-col relative'>
 			<div className='w-full m-auto flex flex-col sm:mt-36 mt-20 c-space gap-3'>
@@ -54,15 +24,18 @@ const Hero = () => {
 				</p>
 			</div>
 			<div className='w-full h-full absolute inset-0'>
-				<Leva />
+				{/* <Leva /> */}
 				<Canvas className='w-full h-full'>
 					<Suspense fallback={<CanvasLoader />}>
-						<PerspectiveCamera makeDefault position={[0, 0, 30]} />
+						<PerspectiveCamera makeDefault position={[0, 0, 20]} />
 						<HackerRoom
-							position={[x.positionX, x.positionY, x.positionZ]}
-							rotation={[x.rotationX, x.rotationY, x.rotationZ]}
-							scale={[x.scale, x.scale, x.scale]}
+							position={sizes.deskPosition}
+							rotation={[0, -Math.PI, 0]}
+							scale={sizes.deskScale}
 						/>
+						<group>
+							<Target position={sizes.targetPosition} />
+						</group>
 						<ambientLight intensity={1} />
 						<directionalLight position={[10, 10, 10]} intensity={0.5} />
 					</Suspense>
