1ad66e445639646215f93e17d0156118d25e0801
0xsambit <177391633+0xsambit@users.noreply.github.com>
2024-09-17 22:05:09 +0530

    Refactor CSS to add custom vertical scrollbar

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -80,11 +80,13 @@ const About = () => {
 							<p className='grid-subtext'>
 								I am based in India, with remote work available
 							</p>
-							<Button
-								name='Contact Me'
-								isBeam
-								containerClass='w-full mt-10 rounded-2xl'
-							/>
+							<a href='#contact'>
+								<Button
+									name='Contact Me'
+									isBeam
+									containerClass='w-full mt-10 rounded-2xl'
+								/>
+							</a>
 						</div>
 					</div>
 				</div>
